8a676458f39ed281cf33e75c7f330227f7a433ff
parix1999 <parisottoedoardo@gmail.com>
2021-09-13 18:23:35 +0200

    grafica

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,17 +20,20 @@
         </div>
     </div>
     <!-- Parte personalizzata da me: -->
-    <div class="sezione-card">
+    <div class="sezione-prodotto">
         <div class="row">
             @foreach($allPosts as $post)
 
-                <div class="col-4">
-                    <div class="card">
+                <div class="col-6 col-sm-6 col-md-4 col-lg-4">
+                    <div class="prodotto">
                         <div class="box-img">
                             <img src="{{ $post['cover'] }}" alt="Immagine compertina di {{ $post['title'] }}">
                         </div>
                         <div class="title">
                             {{ $post['title'] }}
+                        </div>
+                        <div class="description">
+                            {{ $post['abstract'] }}
                         </div>
                     </div>
                 </div>
