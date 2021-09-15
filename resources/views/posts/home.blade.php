749bdf05786ff87675f1b8d236cd92dd460b1b6b
parix1999 <parisottoedoardo@gmail.com>
2021-09-15 15:24:03 +0200

    collegamento

--- resources/views/posts/home.blade.php
+++ resources/views/posts/home.blade.php
@@ -1,54 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+<div class="bg">
+    
+    <div class="container">
+        <!-- <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+    
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+    
+                        {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <!-- Parte personalizzata da me: -->
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Title</th>
-                <th scope="col">Description</th>
-                <th scope="col">Price</th>
-                <th scope="col">Image</th>
-                <th scope="col">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($allPosts as $post)
+        </div> -->
+        <!-- Parte personalizzata da me: -->
+        <table class="table">
+            <thead class="prova">
                 <tr>
-                    <th scope="row">{{ $post->id }}</th>
-                    <td>{{ $post->title }}</td>
-                    <td>{{ $post->abstract }}</td>
-                    <td>{{ $post->price }}</td>
-                    <td>
-                        <img src="{{ $post->cover }}" alt="foto di {{$post->title}}">
-                    </td>
-                    <td><i class="bi bi-door-open"></i></td>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Actions</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-
-
+            </thead>
+            <tbody>
+                @foreach($allPosts as $post)
+                    <tr>
+                        <th scope="row">{{ $post->id }}</th>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->abstract }}</td>
+                        <td>
+                            <img src="{{ $post->cover }}" alt="foto di {{$post->title}}">
+                        </td>
+                        <td><a href="{{ route('posts.show', $post) }}"><i class="bi bi-door-open"></i></a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    
+    
+    
+    </div>
 </div>
 
 @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
 <div class="container">
-    {{ $post->price}}
+
+
+    <table class="table">
+            <thead class="prova">
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+           
+                <tr>
+                    <th scope="row">{{ $post->title}}</th>
+                    <td>{{ $post->price}}</td>
+                    <td>
+                        <img src="{{ $post->cover }}" alt="foto di {{$post->title}}">
+                    </td>
+                    <td><button>Buy</button></td>
+                </tr>
+            
+        </tbody>
+    </table>
 
     
 </div>
+
+@endsection
