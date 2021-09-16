749c4cae00db008325a833566c5f477957dda006
parix1999 <parisottoedoardo@gmail.com>
2021-09-16 18:12:53 +0200

    grafica

--- resources/views/initial.blade.php
+++ resources/views/initial.blade.php
@@ -1,1 +1,22 @@
-ciao
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container-fluid bg-azuro">
+    <div class="container">
+        <div class="title">
+            Guarda tutti i nostri Post Template
+        </div>
+        <div class="bottone">
+            <button type="button" class="btn btn-primary"><a href="/posts">Vai</a></button>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+@endsection('content')
