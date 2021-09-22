6ed1827549a0b5de18e6dca9f6370b270e4509f1
parix1999 <parisottoedoardo@gmail.com>
2021-09-22 16:46:37 +0200

    creazione seeder, con collegamento one to many

--- database/seeds/postsTableSeeder.php
+++ database/seeds/postsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 // C'è da importare il model, dove ci sono i dati del DB
 use App\post;
+use App\Category;
 
 // Ora devo importare il faker cosi da poterlo passare come argomento sul metodo run:
 use Faker\Generator as Faker;
@@ -16,7 +17,28 @@ class postsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-         
+        // Creazione di un'array, che poi sarà popolato in maniera randomica:
+        $listaCategorie = [
+            'Artistico',
+            'Scuro',
+            'Chiaro',
+            'Gotico',
+            'Romano',
+            'Classico',
+            'Moderno',
+            'Rinascimentale'
+        ];
+
+        // Serve l'ID:
+        $listCategoryId = [];
+        // Serve un foreach per iterare dentro all'array:
+        foreach($listaCategorie as $category) {
+            $categoriaObject = new Category();
+            $categoriaObject->genere = $category;
+            // Salviamo: 
+            $categoriaObject->save();  
+            $listCategoryId[] = $categoriaObject->id; // Sto pushando l'id dell'array, quindi diventera 1 2 3 4 5 6 7 8;  
+        }
 
 
         // Funzione per un numero random da mettere dentro il faker per la generazione di parole:
@@ -31,10 +53,17 @@ class postsTableSeeder extends Seeder
             $postObject->cover = $faker->imageUrl(200, 200, 'posts', true);
             $postObject->abstract = $faker->paragraph();
             $postObject->price = $faker->randomFloat(2, 20, 1000);
+            // Qui passo i dati(id) della tabella collegata che saranno dentro la colonna category_id:
+            $randCategoryKey = array_rand($listCategoryId, 1); // Qui mi restituisce la key
+            $categoryID = $listCategoryId[$randCategoryKey];
+            $postObject->category_id = $categoryID; // Cosi mi popola con l'id collegato alla colonna della tabella
+
             $postObject->created_at = null;
             $postObject->updated_at = null;
             // Per passare i dati e salvarli:
             $postObject->save();
+
+
         }
 
     }
